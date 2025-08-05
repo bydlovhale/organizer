8af7f2b96f98dedb4701f933c9a4987ff687c543
Dhruv Bhatt <bdhruv630@gmail.com>
2025-08-05 17:28:00 +0000

    Added README.md with usage details

--- organizer.py
+++ organizer.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-# Change: User prompted for folder aabb
+# Change: User prompted for folder aabbvc
 FOLDER = input("Enter folder path to organize: ")
 
 # Change: Extensions expanded
